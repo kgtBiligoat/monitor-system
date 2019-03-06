bc9c527b90d38c6b2a2a4be1e6d78c703b6914c7
llb <llb@admindeMacBook-Pro-2.local>
2019-03-06 19:47:08 +0800

    注册页面完成

---
+++ src/views/Login/components/SignUp.vue
@@ -0,0 +1,129 @@
+<template>
+    <el-dialog title="sign up" :before-close="closeDialog" :visible.sync="isShow">
+        <el-form label-width="80px" :model="form" ref="elForm" :rules="rules">
+            <el-form-item label="username:" prop="username" required>
+                <el-input v-model="form.username" placeholder="please write your username"></el-input>
+            </el-form-item>
+            <el-form-item label="password:" prop="password" required>
+                <el-input v-model="form.password" placeholder="please write your password" show-password></el-input>
+            </el-form-item>
+            <el-form-item label="checkPassword:" prop="checkPassword" required>
+                <el-input v-model="form.checkPassword" placeholder="please check your password" show-password></el-input>
+            </el-form-item>
+            <el-form-item label="email:">
+                <el-input v-model="form.email" placeholder="please write your email"></el-input>
+            </el-form-item>
+            <el-form-item label="sex:">
+                <el-radio v-model="form.sex" label="man"></el-radio>
+                <el-radio v-model="form.sex" label="women"></el-radio>
+            </el-form-item>
+        </el-form>
+
+        <el-row style="margin-left: 200px;">
+            <el-button plain style="margin-right: 75px;" @click="reset('elForm')">reset</el-button>
+            <el-button plain type="primary" @click="submit">confirm</el-button>            
+        </el-row>
+
+    </el-dialog>
+</template>
+<script lang='ts'>
+import Vue from 'vue';
+import { Component, Prop, Watch } from 'vue-property-decorator';
+
+@Component({
+    data() {
+        var validateUser = (rule: any, value: string, callback: any) => {
+            if (!value.trim()) {
+                console.log(1111)
+                return callback(new Error('password is not allowed be empty'))
+            } else if (/\d|\s/.test(value.trim())) {
+                return callback(new Error('password should be alphabet'))
+            }
+            else {
+                callback()
+            }
+        }
+        var validatePass = (rule: any, value: string, callback: any) => {
+            if (!value) {
+                return callback(new Error('password is not allowed be empty'))
+            } else {
+                callback()
+            }
+        }
+        var validateCheck = (rule: any, value: string, callback: any) => {
+            console.log(this.$data.form.checkPassword ,value)
+            if (!value) {
+                return callback(new Error('password is not allowed be empty'))
+            } else if (this.$data.form.password !== value) {
+                return callback(new Error('The two passwords are inconsistent!'))
+            } else {
+                callback()
+            }
+        }
+        return {
+            isShow: Boolean,
+            form: {
+                username: '',
+                password: '',
+                checkPassword: '',
+                email: '',
+                sex: 'man',
+            },      
+            rules: {
+                username: [
+                    { validator: validateUser, trigger: 'blur' }
+                ],
+                password: [
+                    { validator: validatePass, trigger: 'blur' }
+                ],
+                checkPassword: [
+                    { validator: validateCheck, trigger: 'blur' }
+                ]
+            }     
+        };
+    }
+})
+export default class SignUp extends Vue {
+    @Prop({
+        type: Boolean
+    })
+    isSignUpShow!: boolean
+
+    closeDialog() {
+        this.$data.isShow = false;
+        this.$emit('update:isSignUpShow', this.$data.isShow);
+    }
+
+    @Watch("isSignUpShow", { immediate: true }) 
+    watchIsSignUpShow() {
+        console.log(1)
+        this.$data.isShow = this.isSignUpShow;
+    }
+
+    isEmpty(formName: string) {
+        (this.$refs[formName] as any).validate((valid: any) => {
+            if (valid) {
+                return true
+            } else {
+                return false
+            }
+        });
+    }
+
+    submit() {
+        let isEmpty = this.isEmpty('elForm') as any
+        if(!isEmpty) {
+            console.log('提交')
+        } else {
+            console.log('校验')
+        }
+    }
+
+    reset(formName: string) {
+         (this.$refs[formName] as any).resetFields();
+    }
+}
+</script> 
+
+<style lang='less' scoped>
+</style>
---
+++ src/views/Login/index.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="login" >
+        <img 
+            src="@/assets/login.png" 
+            style="position:absolute;
+            left:30px;
+            top:30px;"
+        >
+        <div class="login-input__group">
+            <el-input placeholder="please input username" v-model="username">
+                <template slot="prepend">username</template>
+            </el-input>
+            <el-input placeholder="please input pasdword" v-model="password" show-password>
+                <template slot="prepend">password</template>
+            </el-input>            
+        </div>
+        <el-row>
+            <el-button plain type="primary" style="margin-right: 50px;" @click="signIn">sign in</el-button>
+            <el-button plain type="primary" @click="signUp">sign up</el-button>
+        </el-row>
+
+        <sign-up v-show="isSignUpShow" :isSignUpShow.sync="isSignUpShow"></sign-up>
+
+    </div>
+</template>
+
+<script lang='ts'>
+import Vue from 'vue';
+import { Component, Prop, Watch } from 'vue-property-decorator';
+
+import SignUp from './components/SignUp.vue'
+
+@Component({
+    data() {
+        return {
+            username: '',
+            password: '',
+            isSignUpShow: false,
+        };
+    },
+    components: { SignUp }
+})
+export default class App extends Vue {
+    signUp() {
+        this.$data.isSignUpShow = true;
+    }
+
+    signIn() {}
+}
+</script>
+
+<style lang='less' scoped>
+.login{
+    background: url(assets/background.jpg);
+    height: 100%;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    .login-input__group {
+        display: flex;
+        flex-direction: column;
+        width: 400px;
+        height: 100px;
+        justify-content: space-between;
+        margin-bottom: 20px;
+        .el-input {
+            width: 400px;
+        }        
+    }
+
+}
+</style>
