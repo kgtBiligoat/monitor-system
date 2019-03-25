e3d84900ac1c2bbb12a637ad509bb64a12e7c265
llb <llb@admindeMacBook-Pro-2.local>
2019-03-25 13:44:14 +0800

    接入地址接入数据库界面玩成

--- src/views/Users/components/getData/GetAddress.vue
+++ src/views/Users/components/getData/GetAddress.vue
@@ -1,14 +1,35 @@
 <template>
-    <div>
-        <el-select v-model="value" placeholder="请选择">
-            <el-option
-                v-for="item in options"
-                :key="item.value"
-                :label="item.label"
-                :value="item.value"
-            >
-            </el-option>
-        </el-select>
+    <div class="getAddress">
+        <el-card>
+            <div slot="header" >
+                <span>接入地址</span>
+            </div>
+            <div class="row">
+                <label>当前代理：localhost:{{value}}</label>
+            </div>
+            <div class="row">
+                <label>代理选择：</label>
+                <el-select v-model="value" placeholder="请选择地址">
+                    <el-option
+                        v-for="item in options"
+                        :key="item.value"
+                        :label="item.label"
+                        :value="item.value"
+                    >
+                    </el-option>
+                </el-select>            
+            </div>
+            <div class="row">
+                <el-table
+                    border
+                >
+                    <el-table-column type="expand"></el-table-column>
+                    <el-table-column label="地址名"></el-table-column>
+                    <el-table-column label="描述"></el-table-column>
+                </el-table>                 
+            </div>
+           
+        </el-card>
     </div>
 </template>
 
@@ -18,7 +39,7 @@ import { Component, Prop, Watch } from 'vue-property-decorator';
 @Component({
     data() {
         return {
-            options: [{value: 1, label: 2}],
+            options: [{value: 3000, label: 'localhost:3000'}],
             value: ''
         }
     }
@@ -27,4 +48,19 @@ export default class getAddress extends Vue {}
 </script>
 
 <style lang='less' scoped>
+.getAddress {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    flex-direction: column;
+    padding: 0px 10%;
+    .row {
+        margin-bottom: 20px;
+        .el-table {
+            min-height: 300px;
+        }
+    }
+}
 </style>
--- src/views/Users/components/getData/GetDataBase.vue
+++ src/views/Users/components/getData/GetDataBase.vue
@@ -1,15 +1,65 @@
 <template>
-    <div>
-        test getDataBase
+    <div class="getDataBase">
+        <el-card>
+            <div slot="header" >
+                <span>接入数据库</span>
+            </div>
+            <div class="row">
+                <label>当前数据库：</label>
+            </div>
+            <div class="row">
+                <label>数据库选择：</label>
+                <el-select v-model="value" placeholder="请选择数据库">
+                    <el-option
+                        v-for="item in options"
+                        :key="item.value"
+                        :label="item.label"
+                        :value="item.value"
+                    >
+                    </el-option>
+                </el-select>            
+            </div>
+            <div class="row">
+                <el-table
+                    border
+                >
+                    <el-table-column type="expand"></el-table-column>
+                    <el-table-column label="数据库名"></el-table-column>
+                    <el-table-column label="描述"></el-table-column>
+                </el-table>                 
+            </div>
+        </el-card>
     </div>
 </template>
 
 <script lang='ts'>
 import Vue from 'vue';
 import { Component, Prop, Watch } from 'vue-property-decorator';
-@Component
+@Component({
+    data() {
+        return {
+            options: [{value: 3000, label: 'Users'}],
+            value: ''
+        }
+    }
+})
 export default class getDataBase extends Vue {}
 </script>
 
 <style lang='less' scoped>
+.getDataBase {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    flex-direction: column;
+    padding: 0px 10%;
+    .row {
+        margin-bottom: 20px;
+        .el-table {
+            min-height: 300px;
+        }
+    }
+}
 </style>
